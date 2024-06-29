7f22fac6ae14f5fae5279dbab62b93bacb23d5da
Hailey <haileyrpan@gmail.com>
2024-06-29 17:43:14 -0400

    navbar and finish outline of home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,33 +17,33 @@ function App() {
         </h1>
       </div>
       <div className="flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-10 mb-24 mx-10 md:mx-20">
-        <div className="w-full md:max-w-48 text-center">
+        <div className="w-full max-w-48 text-center">
           <h2 className="font-lora text-custom-blue-1 text-5xl md:text-7xl mb-4">
             37
           </h2>
-          <h2 className="font-lora text-custom-blue-1 text-md">
+          <h2 className="font-lora text-accent-blue text-md">
             countries our investors are based in
           </h2>
         </div>
-        <div className="w-full md:max-w-48 text-center">
+        <div className="w-full max-w-48 text-center">
           <h2 className="font-lora text-custom-blue-1 text-5xl md:text-7xl mb-4">
             240
           </h2>
-          <h2 className="font-lora text-custom-blue-1 text-md">
+          <h2 className="font-lora text-accent-blue text-md">
             industry-leading investors in our funds
           </h2>
         </div>
-        <div className="w-full md:max-w-48 text-center">
+        <div className="w-full max-w-48 text-center">
           <h2 className="font-lora text-custom-blue-1 text-5xl md:text-7xl mb-4">
             1.2M
           </h2>
-          <h2 className="font-lora text-custom-blue-1 text-md">
+          <h2 className="font-lora text-accent-blue text-md">
             people employed by our investors' brands
           </h2>
         </div>
       </div>
-      <div className="flex justify-center items-center max-w-4xl h-full mx-auto mt-20 md:mt-40 mb-20 md:mb-40 px-4">
-        <p className="font-lora text-custom-blue-1 text-md text-center">
+      <div className="flex justify-center items-center max-w-4xl h-full mx-auto mt-20 md:mt-24 mb-20 md:mb-40">
+        <p className="font-lora text-accent-blue text-md text-center max-w-96">
           Y&Z Ventures is a Los Angeles based multi-family office, operating as
           a direct investment vehicle + fund of funds. We are dedicated to
           supporting investments to the final stages, or the end game. Through
@@ -53,49 +53,33 @@ function App() {
         </p>
       </div>
       <div className="mx-10 md:mx-20 mt-20 mb-20">
-        <h2 className="text-center font-lora text-custom-blue-1 text-3xl md:text-4xl mb-10">Partner Industries</h2>
-        <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 text-custom-blue-1 border-custom-blue-1 border-t-2 border-b-2 pt-10 pb-10">
-          <div className="text-center">
+        <h2 className="text-center font-lora text-custom-blue-1 text-2xl md:text-4xl mb-10">
+          Partner Industries
+        </h2>
+        <div className="max-w-3xl font-lora text-lg mx-auto grid grid-cols-1 gap-10 text-custom-blue-1 border-custom-blue-1 border-t border-b pt-10 pb-10 md:grid-cols-2">
+          <div className="text-center md:border-r border-custom-blue-1">
             <ul>
-              <li>Engineering</li>
-              <li>Construction</li>
-              <li>Real Estate</li>
-              <li>Security</li>
               <li>Agriculture</li>
-              <li>Media & Entertainment</li>
-              <li>Transportation</li>
-              <li>Textiles</li>
-              <li>Blockchain</li>
-            </ul>
-          </div>
-          <div className="text-center">
-            <ul>
-              <li>Oil & Gas</li>
-              <li>Finance</li>
-              <li>Aviation</li>
               <li>Education</li>
-              <li>Food & Beverage</li>
-              <li>IT</li>
-              <li>Retail</li>
-              <li>Chemical</li>
+              <li>Healthcare</li>
               <li>Logistics</li>
-              <li>Storage</li>
             </ul>
           </div>
           <div className="text-center">
             <ul>
-              <li>Materials</li>
-              <li>Hospitality</li>
-              <li>Telecom</li>
-              <li>Energy</li>
-              <li>Automotive</li>
-              <li>Healthcare</li>
-              <li>Pharma</li>
-              <li>Fashion</li>
-              <li>Aerospace</li>
+              <li>Real Estate</li>
+              <li>Supply Chain</li>
+              <li>Transportation</li>
             </ul>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center items-center my-40">
+        <a href="https://airtable.com/appIqybHNdQCLdf41/shrSpqTrmw8KANvBr" target="_blank">
+          <button className="border-2 border-custom-blue-1 py-2 px-4 font-lora text-custom-blue-1 text-center text-2xl md:text-4xl">
+            Pitch us
+          </button>
+        </a>
       </div>
     </>
   );
--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,20 +1,113 @@
-import React from 'react';
-import logo from './assets/yz_logo.png';
-import './assets/yz_logo.png';
+import React, { useState } from "react";
+import logo from "./assets/yz_logo.png";
+import "./assets/yz_logo.png";
 
 const Navbar = () => {
-    return (
-      <div className="flex items-center justify-between px-16 py-6 text-white">
-        <div className="flex-shrink-0">
-          <img src={logo} className="h-14" alt="logo" />
-        </div>
-        <div className="flex space-x-10">
-          <a href="#mission" className="font-lora text-custom-blue-1 hover:text-gray-400">Mission</a>
-          <a href="#about" className="font-lora text-custom-blue-1 hover:text-gray-400">About</a>
-          <a href="#contact" className="font-lora text-custom-blue-1 hover:text-gray-400">Contact</a>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img src={logo} className="h-14" alt="logo" />
+            </div>
+          </div>
+          <div className="hidden md:flex space-x-10">
+            <a
+              href="#mission"
+              className="font-lora text-custom-blue-1 hover:text-gray-400"
+            >
+              Mission
+            </a>
+            <a
+              href="#about"
+              className="font-lora text-custom-blue-1 hover:text-gray-400"
+            >
+              About
+            </a>
+            <a
+              href="#contact"
+              className="font-lora text-custom-blue-1 hover:text-gray-400"
+            >
+              Contact
+            </a>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className=" inline-flex items-center justify-center p-2 rounded-md text-custom-blue-1 hover:text-accent-blue focus:outline-none focus:text-accent-blue"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
       </div>
-    );
-  };
-  
-  export default Navbar;
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <a
+              href="#mission"
+              className="block px-3 py-2 rounded-md text-base font-medium text-center text-custom-blue-1 hover:text-accent-blue"
+            >
+              Mission
+            </a>
+            <a
+              href="#about"
+              className="block px-3 py-2 rounded-md text-base font-medium text-center text-custom-blue-1 hover:text-accent-blue"
+            >
+              About
+            </a>
+            <a
+              href="#contact"
+              className="block px-3 py-2 rounded-md text-base font-medium text-center text-custom-blue-1 hover:text-accent-blue"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
